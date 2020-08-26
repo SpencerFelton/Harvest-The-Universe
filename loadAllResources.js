1bae9cf81c6f52a554e9aa62be354c9933c085dd
Spencer Felton <SpencerEFelton@gmail.com>
2020-08-26 22:50:37 +0100

    New Resource Added
    Tree

--- loadAllResources.js
+++ loadAllResources.js
@@ -4,7 +4,8 @@ resourceInfoJSON = [
   {"id":0, "name":"Man", "hp":10, "defence":0, "reward":1, "url":resourceLocation+"human.png"},
   {"id":1, "name":"Long Man", "hp":100, "defence":1, "reward":5, "url":resourceLocation+"slinkyHuman.png"},
   {"id":2, "name":"Longest Man", "hp":250, "defence":5, "reward":10, "url":resourceLocation+"longestBoi.png"},
-  {"id":3, "name":"Buff Fella", "hp":1000, "defence":10, "reward":100, "url":resourceLocation+"legDay.png"}
+  {"id":3, "name":"Buff Fella", "hp":1000, "defence":10, "reward":100, "url":resourceLocation+"legDay.png"},
+  {"id":4, "name":"Smol Tree", "hp":2000, "defence":50, "reward":150, "url":resourceLocation+"tree.png"}
 ]
 
 function loadResources(array){ // create Resource objects from an array of json objects, returns an array of Resources
